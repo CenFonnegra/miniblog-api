cac00100b433c6852f85c95827eaab9b7efa3ab1
CenFonnegra <willy1323@hotmail.com>
2026-05-11 17:56:40 -0500

    feat: add get by id and update author endpoints

--- src/controllers/authors.controller.js
+++ src/controllers/authors.controller.js
@@ -51,7 +51,81 @@ const getAuthors = async (req, res) => {
 
 };
 
+const getAuthorsById = async (req, res) => {
+    const { id } = req.params; // => ayuda a obtener parametros de la URL
+
+    try {
+
+        const result = await pool.query(
+            'SELECT * FROM authors WHERE id = $1', // => Buscamos autor especifico
+            [id]
+        );
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                error: 'Author not found'
+            });
+        }
+
+        res.status(200).json(result.rows[0]);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+
+    }
+
+};
+
+const updateAuthor = async (req, res) => {
+    
+    const { id } = req.params; //=> obtenemos el Id
+
+    const { name, email, bio } = req.body; //=> obtenemos nuevos datos
+
+    if (!name || !email) {
+        return res.status(400).json({
+            error: 'Name and email are required'
+        });
+    }
+
+    try {
+
+        const result = await pool.query(
+            `UPDATE authors
+            SET name = $1, email = $2, bio = $3
+            WHERE id = $4
+            RETURNING *`, // => Devuelve el autor actualizado
+            [name, email, bio, id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                error: 'Author not found'
+            });
+        }
+
+        res.status(200).json(result.rows[0]);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+    }
+
+    
+
+};
+
 module.exports = {
     createAuthor,
-    getAuthors
+    getAuthors,
+    getAuthorsById,
+    updateAuthor
 };
--- src/routes/authors.routes.js
+++ src/routes/authors.routes.js
@@ -2,8 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 
-const { createAuthor } = require('../controllers/authors.controller');
+const {
+     createAuthor,
+     getAuthors,
+     getAuthorsById,
+     updateAuthor
+     } = require('../controllers/authors.controller');
 
 router.post('/', createAuthor);
+
+router.get('/', getAuthors);
+
+router.get('/:id', getAuthorsById); 
+
+router.put('/:id', updateAuthor);
 
 module.exports = router;
