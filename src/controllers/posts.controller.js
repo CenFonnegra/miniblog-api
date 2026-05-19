14b7a539b004e1b8a5a0ba79ba879bb0e2088efd
CenFonnegra <willy1323@hotmail.com>
2026-05-18 20:17:13 -0500

    feat: complete CRUD for posts and authors

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -30,6 +30,165 @@ const createPost = async (req, res) => {
 
 };
 
+const getPosts = async (req, res) => {
+
+    try {
+
+        const result = await pool.query(
+            'SELECT * FROM posts'
+        );
+
+        res.status(200).json(result.rows);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+
+    }
+};
+
+const getPostsById = async (req, res) => {
+
+    const { id } = req.params; // => ayuda a obtener parametros de la URL
+
+    try {
+
+        const result = await pool.query(
+            'SELECT * FROM posts WHERE id = $1', // => Buscamos posts especificos
+            [id]
+        );
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                error: 'Post not found'
+            });
+        }
+
+        res.status(200).json(result.rows[0]);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+
+    }
+
+};
+
+const getPostsByAuthor = async (req, res) => {
+
+    const {authorId} = req.params; 
+
+    try {
+
+        const result = await pool.query(
+            'SELECT posts.id AS post_id, posts.title, posts.content, authors.name AS author_name, authors.email AS author_email FROM posts JOIN authors ON posts.author_id = authors.id WHERE authors.id = $1;', // => Buscamos posts especificos
+            [authorId]
+        );
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                error: 'Post not found'
+            });
+        }
+
+        res.status(200).json(result.rows); // solo .rows ya que devuelve muchos posts
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+
+    }
+
+};
+
+const updatePost = async (req, res) => {
+
+    const { id } = req.params; //=> obtenemos el Id
+
+    const { author_id, title, content, published } = req.body; //=> obtenemos nuevos datos
+
+    if (!author_id || !title || !content) {
+        return res.status(400).json({
+            error: 'author_id,  title and content are required'
+        });
+    }
+
+    try {
+
+        const result = await pool.query(
+            `UPDATE posts
+            SET author_id = $1, title = $2, content = $3, published = $4
+            WHERE id = $5
+            RETURNING *`, // => Devuelve el autor actualizado
+            [author_id, title, content, published, id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                error: 'Post not found'
+            });
+        }
+
+        res.status(200).json(result.rows[0]);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+    }
+
+};
+
+const deletePost = async (req, res) => {
+
+    const { id } = req.params;  
+
+    try {
+
+        const result = await pool.query(
+            'DELETE FROM posts WHERE id = $1 RETURNING *',
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                error: 'Post not found'
+            });
+        }
+
+        res.status(200).json({
+            message: 'Post deleted succesfully'
+        });
+ 
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+    }
+
+};
+
 module.exports = {
-    createPost
+    createPost,
+    getPosts, 
+    getPostsById,
+    getPostsByAuthor, 
+    updatePost, 
+    deletePost
 };
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -3,9 +3,24 @@ const express = require('express');
 const router = express.Router();
 
 const {
-     createPost
+     createPost,
+     getPosts,
+     getPostsById, 
+     getPostsByAuthor,
+     updatePost,
+     deletePost
      } = require('../controllers/posts.controller');
 
 router.post('/', createPost);
+
+router.get('/', getPosts);
+
+router.get('/author/:authorId', getPostsByAuthor);
+
+router.get('/:id', getPostsById);
+
+router.put('/:id', updatePost);
+
+router.delete('/:id', deletePost);
 
 module.exports = router;
